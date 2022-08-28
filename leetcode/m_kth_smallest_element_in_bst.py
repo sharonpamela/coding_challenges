a4e760040b1c9f0ff00a3adb3f68024fe3a0738a
sharonpamela <sharon.pamela.santana@gmail.com>
2022-08-28 14:29:57 -0500

    m_kth_smallest_element_in_bst

---
+++ leetcode/m_kth_smallest_element_in_bst.py
@@ -0,0 +1,43 @@
+'''
+Given the root of a binary search tree, and an integer k, 
+return the kth smallest value (1-indexed) of all the 
+values of the nodes in the tree.
+'''
+
+def kthSmallest(root, k):
+   
+    n = 0
+    stack = []
+    cur = root
+
+    while stack:
+    in_order_traversal(root, res)
+    if len(res) >= k-1:
+        return res[k-1]
+    else:
+        return -1
+
+def in_order_traversal(root, res):
+    if root.left:
+        in_order_traversal(root.left)
+    res.append(root.val)
+    if root.right:
+        in_order_traversal(root.right)
+
+
+# def kthSmallest(root, k):
+#     # do an in-order traversal of the tree
+#     # this returns the nodes in order
+#     res = []
+#     in_order_traversal(root, res)
+#     if len(res) >= k-1:
+#         return res[k-1]
+#     else:
+#         return -1
+
+# def in_order_traversal(root, res):
+#     if root.left:
+#         in_order_traversal(root.left)
+#     res.append(root.val)
+#     if root.right:
+#         in_order_traversal(root.right)
