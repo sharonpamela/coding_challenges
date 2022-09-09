a6d91b523e16a9532be26e2d53c0217931a6a4f0
sharonpamela <sharon.pamela.santana@gmail.com>
2022-09-08 23:18:32 -0500

    .

---
+++ leetcode/e_190_reverse_bits.py
@@ -0,0 +1,10 @@
+'''
+Reverse bits of a given 32 bits unsigned integer
+'''
+
+def reverseBits(n):
+    res = 0
+    for i in range(32):
+        bit = (n >> i) & 1
+        res = res | (bit << (31 -i))
+    return res
