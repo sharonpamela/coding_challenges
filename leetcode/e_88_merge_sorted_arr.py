558e125440c5dcdcd8b197a980924b776181757f
sharonpamela <sharon.pamela.santana@gmail.com>
2022-09-21 21:16:56 -0500

    .

--- leetcode/e_88_merge_sorted_arr.py
+++ leetcode/e_88_merge_sorted_arr.py
@@ -1,11 +1,13 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        # Write the elements of num2 into the end of nums1.
-        for i in range(n):
-            nums1[i + m] = nums2[i]
-        
-        # Sort nums1 list in-place.
-        nums1.sort()
+def merge(nums1, m, nums2, n):
+    n1 = m-1
+    n2 = n-1
+    for i in range(len(nums1)-1, -1, -1):
+        if n2 <0 :
+            break
+            
+        if n1 >= 0 and nums1[n1] > nums2[n2]:
+            nums1[i]=nums1[n1]
+            n1-=1
+        else:
+            nums1[i]=nums2[n2]
+            n2-=1
